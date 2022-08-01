2016b8546076327202d20fde5c38a0f1c466bccf
MileySJ <ohmiley007@gmail.com>
2022-08-02 00:41:06 +0100

    html

--- recipes/models.py
+++ recipes/models.py
@@ -8,10 +8,16 @@ class Asian(models.Model):
     ingrediants = models.TextField(max_length=500)
     description = models.TextField(max_length=800)
 
+    def __str__(self):
+        return self.title
+
 class Italian(models.Model):
     title = models.CharField(max_length=300)
     image = models.ImageField(blank=True)
     ingrediants = models.TextField(max_length=500)
     description = models.TextField(max_length=800)
+
+    def __str__(self):
+        return self.title
 
     
