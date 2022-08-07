cdfaf12caa00d31f3a2c748f671c31c355afbfff
MileySJ <ohmiley007@gmail.com>
2022-08-08 00:33:58 +0100

    searchbox &result page using queryset filter&chain

--- recipes/urls.py
+++ recipes/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('asian/<int:asian_id>', views.asian_detail, name='asian_detail'),
     path('italian/<int:italian_id>', views.italian_detail, name='italian_detail'),
     path('about/', views.about, name='about'),
+    path('research/', views.research, name='research'),
+    path('recipe/', views.recipe, name='recipe'),
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,6 @@
+from itertools import chain
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from .models import Asian, Italian
 
 
@@ -23,3 +25,18 @@ def italian_detail(request, italian_id):
 
 def about(request):
     return render(request, 'recipes/about.html')
+
+def research(request):
+    return render(request, 'recipes/search.html')
+
+
+def recipe(request):
+
+    search = request.GET.get('search')
+
+    recipe_asian = Asian.objects.filter(title__icontains=search)
+    recipe_italian = Italian.objects.filter(title__icontains=search)
+    recipes = chain(recipe_asian, recipe_italian)
+    return render(request, 'recipes/recipe.html', {'recipes':recipes})
+
+    
